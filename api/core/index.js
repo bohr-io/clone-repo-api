c7b95bcd5390c31f24dfbc0706b3d0fe1bb6cfc2
Lucas Boemeke <lucas@blueshift.cc>
2023-07-29 17:31:18 -0300

    initial commit

--- api/core/index.js
+++ api/core/index.js
@@ -1,8 +1,25 @@
-exports.handler = async (event) => {
-    console.log('Hello World!');
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello World!'),
-    };
-    return response;
+const lambda = require('lambda-api');
+
+const api = lambda({
+  cors: true,
+  corsAllowOrigin: '*'
+});
+
+api.get('/', async (req, res) => {
+  return { status: 'ok' };
+});
+
+api.post('/clone', async (req, res) => {
+  const BOHR_CLONE_API_TOKEN = req.body.BOHR_CLONE_API_TOKEN;
+  const GITHUB_TOKEN = req.body.GITHUB_TOKEN;
+  const REPO_OWNER = req.body.REPO_OWNER;
+  const REPO_NAME = req.body.REPO_NAME;
+
+  if ((!process.env.BOHR_CLONE_API_TOKEN) || (process.env.BOHR_CLONE_API_TOKEN != BOHR_CLONE_API_TOKEN)) return { statusCode: 401 };
+
+  return { status: 'ok clone' };
+});
+
+exports.handler = async (event, context) => {
+  return await api.run(event, context);
 };

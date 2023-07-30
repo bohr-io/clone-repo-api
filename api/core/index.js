9be7a0fbe707b58edf4c94ce70af92f09df4649b
Lucas Boemeke <lucas@blueshift.cc>
2023-07-30 00:22:40 -0300

    fix

--- api/core/index.js
+++ api/core/index.js
@@ -7,62 +7,71 @@ const api = lambda({
 });
 
 api.post('/clone', async (req, res) => {
-  const BOHR_CLONE_API_TOKEN = req.body.BOHR_CLONE_API_TOKEN;
-  const GITHUB_ACCESS_TOKEN = req.body.githubAccessToken;
-  const GITHUB_APPLICATION_TOKEN = req.body.githubApplicationToken;
-  const REPO_OWNER = req.body.REPO_OWNER;
-  const REPO_NAME = req.body.REPO_NAME;
 
-  if ((!process.env.BOHR_CLONE_API_TOKEN) || (process.env.BOHR_CLONE_API_TOKEN != BOHR_CLONE_API_TOKEN)) return { statusCode: 401 };
+  console.log('POST /clone');
 
-  if ((REPO_OWNER == 'bohr-repos') || (REPO_OWNER == 'bohr-io')) return { statusCode: 200 };
+  try {
+    const BOHR_CLONE_API_TOKEN = req.body.BOHR_CLONE_API_TOKEN;
+    const GITHUB_ACCESS_TOKEN = req.body.githubAccessToken;
+    const GITHUB_APPLICATION_TOKEN = req.body.githubApplicationToken;
+    const REPO_OWNER = req.body.REPO_OWNER;
+    const REPO_NAME = req.body.REPO_NAME;
 
-  const dest_repo = REPO_OWNER + '-' + REPO_NAME;
+    if ((!process.env.BOHR_CLONE_API_TOKEN) || (process.env.BOHR_CLONE_API_TOKEN != BOHR_CLONE_API_TOKEN)) return { statusCode: 401 };
 
-  const access_token = process.env.GH_ACCESS_TOKEN;
-  const headers = {
-    'accept': 'application/vnd.github+json',
-    'Authorization': 'token ' + access_token,
-    'user-agent': 'bohr-api-lambda'
-  };
+    if ((REPO_OWNER == 'bohr-repos') || (REPO_OWNER == 'bohr-io')) return { statusCode: 200 };
 
-  const response_generate = await fetch(
-    'https://api.github.com/repos/bohr-repos/base/generate',
-    {
-      method: 'POST',
-      body: JSON.stringify({
-        owner: 'bohr-repos',
-        name: dest_repo,
-        private: true
-      }),
-      headers: headers
-    }
-  );
+    const dest_repo = REPO_OWNER + '-' + REPO_NAME;
 
-  await new Promise(s => setTimeout(s, 10000));
+    const access_token = process.env.GH_ACCESS_TOKEN;
+    const headers = {
+      'accept': 'application/vnd.github+json',
+      'Authorization': 'token ' + access_token,
+      'user-agent': 'bohr-api-lambda'
+    };
 
-  const response_dispatches = await fetch(
-    'https://api.github.com/repos/bohr-repos/' + dest_repo + '/dispatches',
-    {
-      method: 'POST',
-      body: JSON.stringify({
-        event_type: 'bohr-clone-event',
-        client_payload: {
-          GITHUB_ACCESS_TOKEN: GITHUB_ACCESS_TOKEN,
-          GITHUB_APPLICATION_TOKEN: GITHUB_APPLICATION_TOKEN,
-          REPO_OWNER: REPO_OWNER,
-          REPO_NAME: REPO_NAME
-        }
-      }),
-      headers: headers
-    }
-  );
+    const response_generate = await fetch(
+      'https://api.github.com/repos/bohr-repos/base/generate',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          owner: 'bohr-repos',
+          name: dest_repo,
+          private: true
+        }),
+        headers: headers
+      }
+    );
 
-  return {
-    status: 'ok2',
-    generate: response_generate.status,
-    dispatches: response_dispatches.status
-  };
+    await new Promise(s => setTimeout(s, 10000));
+
+    const response_dispatches = await fetch(
+      'https://api.github.com/repos/bohr-repos/' + dest_repo + '/dispatches',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          event_type: 'bohr-clone-event',
+          client_payload: {
+            GITHUB_ACCESS_TOKEN: GITHUB_ACCESS_TOKEN,
+            GITHUB_APPLICATION_TOKEN: GITHUB_APPLICATION_TOKEN,
+            REPO_OWNER: REPO_OWNER,
+            REPO_NAME: REPO_NAME
+          }
+        }),
+        headers: headers
+      }
+    );
+
+    return {
+      status: 'ok',
+      generate: response_generate.status,
+      dispatches: response_dispatches.status
+    };
+      
+  } catch (error) {
+    console.error(error);
+    return { status: 'fail' };
+  }
 });
 
 exports.handler = async (event, context) => {
